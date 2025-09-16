d9dede6ff0db01f5e47d828c9c4c570bece5e2ad
Antonella Choque Goljanek <antocg101@gmail.com>
2025-09-16 20:22:37 -0300

    Código final app.js

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nombre = inputAmigo.value.trim();
+    
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+    
+    amigos.push(nombre);
+    
+    inputAmigo.value = '';
+  
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    
+    listaAmigos.innerHTML = '';
+    
+    for (let i = 0; i < amigos.length; i++) {
+        const elementoLista = document.createElement('li');
+        elementoLista.textContent = amigos[i];
+        listaAmigos.appendChild(elementoLista);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos en la lista. Por favor, añada al menos un amigo.');
+        return;
+    }
+    
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    
+    const amigoSorteado = amigos[indiceAleatorio];
+    
+    const resultadoElement = document.getElementById('resultado');
+    resultadoElement.innerHTML = `<li>${amigoSorteado}</li>`;
+}
